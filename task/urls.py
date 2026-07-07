a35be3dc9bd56aad65813451af9fcab3c7e3057c
Aromal Satheesh <aromalsatheesh802003@gmail.com>
2026-07-07 09:05:12 +0530

    all types of views

--- task/urls.py
+++ task/urls.py
@@ -1,13 +1,27 @@
 from django.urls import path  
 from .views import *
 
-urlpatterns = [
-    path('',home.as_view()),
-    path('delete_student/<int:get_id>/',remove_student.as_view()),
-    path('alter_student/<int:id>/',update_student.as_view()),
-    path('get_one_student/<int:get_id>/',get_one_student.as_view()),
-    path('search/',search_data.as_view())
-]
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+
+router.register('', home)
+urlpatterns = router.urls
+
+# urlpatterns = [
+
+#    
+
+#     path('',home.as_view()),
+#     # path('',home),
+
+
+#     path('delete_student/<int:get_id>/',remove_student.as_view()),
+#     path('alter_student/<int:id>/',update_student.as_view()),
+#     path('get_one_student/<int:get_id>/',get_one_student.as_view()),
+#     path('search/',search_data.as_view())
+# ]
 
 
 
--- task/views.py
+++ task/views.py
@@ -1,132 +1,218 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Task
 from .serializer import TaskSerializer
+
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
+from rest_framework.generics import GenericAPIView
+from rest_framework import mixins
+from rest_framework.viewsets import ModelViewSet
 # Create your views here.
 
+# model view set
+class home(ModelViewSet):
 
-class home(APIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
-    def get(self,request):
 
-        get_task = Task.objects.all()
-        get_count = get_task.count()
-        serializer = TaskSerializer(get_task,many=True)
+
+# Generic api views with mixins
+# class home(
+#     mixins.ListModelMixin,
+#     mixins.CreateModelMixin,
+#     GenericAPIView
+# ):
+    
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+
+#     def get(self, request):
+#         return self.list(request)
+    
+#     def post(self, request):
+#         return self.create(request)
+
+# Generic api views
+# class home(GenericAPIView):
+
+#     queryset = Task.objects.all()
+#     # get_count = queryset.count()
+#     serializer_class = TaskSerializer
+
+#     def get(self,request):
+#         task = self.get_queryset()
+#         count = self.get_queryset().count()
+#         serializer = self.get_serializer(task, many=True)
+
+#         return Response({
+#            'message':'successfully got data',            
+#            'data':serializer.data,
+#            'count':count
+#        },status=status.HTTP_200_OK)
         
-        return Response({
-            'message':'successfully got data',
-            'data':serializer.data,
-            'count':get_count
-        },status=status.HTTP_200_OK)
+
+
+ 
+
+
+
+
+# function Based view
+# @api_view(['GET','POST'])
+# def home(request):
+
+#     if request.method == 'POST':
+
+#         serializer = TaskSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({
+#                 'message':'successfully data created',
+#                 'data':serializer.data
+#             }, status=status.HTTP_201_CREATED)
+        
+#         return Response({
+#             'message':'all fileds are requried',
+#             'errors':serializer.errors
+#         }, status=status.HTTP_400_BAD_REQUEST)
     
 
-    def post(self,request):
-        serializer = TaskSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({
-                'message':'successfully data created',
-                'data':serializer.data
-            }, status=status.HTTP_201_CREATED)
+#     get_task = Task.objects.all()
+#     get_count = get_task.count()
+#     serializer = TaskSerializer(get_task,many=True)
+
+#     return Response({
+#             'message':'successfully got data',
+#             'data':serializer.data,
+#             'count':get_count
+#         },status=status.HTTP_200_OK)
+
+
+# class based views 
+# class home(APIView):
+
+#     def get(self,request):
+
+#         get_task = Task.objects.all()
+#         get_count = get_task.count()
+#         serializer = TaskSerializer(get_task,many=True)
         
-        return Response({
-            'message':'all fileds are requried',
-            'errors':serializer.errors
-        }, status=status.HTTP_400_BAD_REQUEST)
+#         return Response({
+#             'message':'successfully got data',
+#             'data':serializer.data,
+#             'count':get_count
+#         },status=status.HTTP_200_OK)
     
 
-class remove_student(APIView):
-
-    def delete(self,request,get_id):
-
-        try:
-            student = Task.objects.get(id=get_id)
-            student.delete()
-            return Response({
-                'message':f'The student {get_id} has deleted from Database'
-            }, status=status.HTTP_200_OK)
+#     def post(self,request):
+#         serializer = TaskSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({
+#                 'message':'successfully data created',
+#                 'data':serializer.data
+#             }, status=status.HTTP_201_CREATED)
         
-        except Task.DoesNotExist:
-            return Response({
-                'message':f'The student {get_id} Does not exist on database'
-            }, status=status.HTTP_404_NOT_FOUND)
+#         return Response({
+#             'message':'all fileds are requried',
+#             'errors':serializer.errors
+#         }, status=status.HTTP_400_BAD_REQUEST)
+    
+
+# class remove_student(APIView):
+
+#     def delete(self,request,get_id):
+
+#         try:
+#             student = Task.objects.get(id=get_id)
+#             student.delete()
+#             return Response({
+#                 'message':f'The student {get_id} has deleted from Database'
+#             }, status=status.HTTP_200_OK)
+        
+#         except Task.DoesNotExist:
+#             return Response({
+#                 'message':f'The student {get_id} Does not exist on database'
+#             }, status=status.HTTP_404_NOT_FOUND)
         
 
-class update_student(APIView):
+# class update_student(APIView):
 
-    def put(self,request,id):
+#     def put(self,request,id):
 
-        try:
-            get_student = Task.objects.get(id=id)
-            serializer = TaskSerializer(instance=get_student, data=request.data)
+#         try:
+#             get_student = Task.objects.get(id=id)
+#             serializer = TaskSerializer(instance=get_student, data=request.data)
 
-            # get_student.name = request.POST.get('name')
+#             # get_student.name = request.POST.get('name')
 
-            if serializer.is_valid():
-                serializer.save()
-                return Response({
-                    'message':f'The student{id} has updated',
-                    'data':serializer.data
-                }, status=status.HTTP_200_OK)
+#             if serializer.is_valid():
+#                 serializer.save()
+#                 return Response({
+#                     'message':f'The student{id} has updated',
+#                     'data':serializer.data
+#                 }, status=status.HTTP_200_OK)
             
-            return Response({
-                'message':'something happend with the input field',
-                'error':serializer.errors
-            }, status=status.HTTP_400_BAD_REQUEST)
+#             return Response({
+#                 'message':'something happend with the input field',
+#                 'error':serializer.errors
+#             }, status=status.HTTP_400_BAD_REQUEST)
         
-        except Task.DoesNotExist:
-            return Response({
-                'message':f'The id{id} does not exist on Database'
-            }, status=status.HTTP_404_NOT_FOUND)
+#         except Task.DoesNotExist:
+#             return Response({
+#                 'message':f'The id{id} does not exist on Database'
+#             }, status=status.HTTP_404_NOT_FOUND)
         
 
-class get_one_student(APIView):
+# class get_one_student(APIView):
 
-    def get(self,request,get_id):
+#     def get(self,request,get_id):
 
-        try:
-            get_student = Task.objects.get(id=get_id)
-            serializer = TaskSerializer(get_student)
+#         try:
+#             get_student = Task.objects.get(id=get_id)
+#             serializer = TaskSerializer(get_student)
             
-            return Response({
-                'message':f'The student id {get_id} has succcesfully fetched from database',
-                'data':serializer.data
-            }, status=status.HTTP_200_OK)
+#             return Response({
+#                 'message':f'The student id {get_id} has succcesfully fetched from database',
+#                 'data':serializer.data
+#             }, status=status.HTTP_200_OK)
         
-        except Task.DoesNotExist:
-            return Response({
-                'message':f'The student id{get_id} Does not exist on Database',
-            }, status=status.HTTP_404_NOT_FOUND)
+#         except Task.DoesNotExist:
+#             return Response({
+#                 'message':f'The student id{get_id} Does not exist on Database',
+#             }, status=status.HTTP_404_NOT_FOUND)
         
 
-class search_data(APIView):
+# class search_data(APIView):
 
-    def get(self, request):
-        try:
-            keyword = request.GET.get('q')
+#     def get(self, request):
+#         try:
+#             keyword = request.GET.get('q')
 
-            get_student = Task.objects.filter(name__icontains=keyword)   
+#             get_student = Task.objects.filter(name__icontains=keyword)   
 
-            if get_student.exists():
+#             if get_student.exists():
 
-                serializer = TaskSerializer(get_student, many=True)
+#                 serializer = TaskSerializer(get_student, many=True)
 
-                return Response({
-                    'message': f'The student "{keyword}" is present in the database',
-                    'data': serializer.data
-                }, status=status.HTTP_200_OK)
+#                 return Response({
+#                     'message': f'The student "{keyword}" is present in the database',
+#                     'data': serializer.data
+#                 }, status=status.HTTP_200_OK)
 
-            return Response({
-                'message': f'The student "{keyword}" does not exist in the database'
-            }, status=status.HTTP_404_NOT_FOUND)
+#             return Response({
+#                 'message': f'The student "{keyword}" does not exist in the database'
+#             }, status=status.HTTP_404_NOT_FOUND)
 
-        except Exception as e:
-            return Response({
-                'message': 'Something went wrong',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+#         except Exception as e:
+#             return Response({
+#                 'message': 'Something went wrong',
+#                 'error': str(e)
+#             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
         
 # class search_data(APIView):
